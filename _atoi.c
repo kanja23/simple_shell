b92115088e43a7ab3664296db59339062b859d0a
kanja23 <martinkaranja92@gmail.com>
2023-03-13 22:18:05 -0700

    _atoi.c

--- _atoi.c
+++ _atoi.c
@@ -21,12 +21,9 @@ int interactive(info_t *info)
 int is_delim(char c, char *delim)
 {
 	while (*delim)
-
-	if (*delim++ == c)
-
-	return (1);
-
-	return (0);
+                if (*delim++ == c)
+                        return (1);
+        return (0);
 }
 
 /**
@@ -37,13 +34,10 @@ int is_delim(char c, char *delim)
 
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-
-	return (1);
-
-	else
-
-	return (0);
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                return (1);
+        else
+                return (0);
 }
 
 /**
@@ -54,45 +48,28 @@ int _isalpha(int c)
 
 int _atoi(char *s)
 {
+        int i, sign = 1, flag = 0, output;
+        unsigned int result = 0;
 
-	int i, sign = 1, flag = 0, output;
+        for (i = 0; s[i] != '\0' && flag != 2; i++)
 
-	unsigned int result = 0;
+        {
+                if (s[i] == '-')
+                        sign *= -1;
+                if (s[i] >= '0' && s[i] <= '9')
 
-	for (i = 0; s[i] != '\0' && flag != 2; i++)
+                {
+                        flag = 1;
+                        result *= 10;
+                        result += (s[i] - '0');
+                }
+                else if (flag == 1)
+                        flag = 2;
+        }
 
-	{
-
-		if (s[i] == '-')
-
-			sign *= -1;
-
-		if (s[i] >= '0' && s[i] <= '9')
-
-		{
-
-			flag = 1;
-
-			result *= 10;
-
-			result += (s[i] - '0');
-
-		}
-
-		else if (flag == 1)
-
-			flag = 2;
-
-	}
-
-	if (sign == -1)
-
-		output = -result;
-
-	else
-
-		output = result;
-
-	return (output);
-
+        if (sign == -1)
+                output = -result;
+        else
+                output = result;
+        return (output);
 }
